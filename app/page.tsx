adb569a7cb577f38dc7c2facb092a872fde0f3ae
Noel Tesfalidet <58369280+NoelMT@users.noreply.github.com>
2024-09-09 13:55:09 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div className="blue-text">Blu Environment</div>
+      <div className="blue-text">Environment</div>
       <div className="company-name">Bad Coders Inc.</div>
     </main>
   );
